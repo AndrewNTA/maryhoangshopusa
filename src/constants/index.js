a75b15a2e8eb2fbf7557595ee66193cbb2c3fac4
andrew nguyen <andrew.nta87@gmail.com>
2024-07-17 15:45:54 +0700

    [Andrew] Remove mocks

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,33 @@
+import BodyImage from 'static/images/body-care-img.jpg';
+import HairImage from 'static/images/hair-care-img.jpg';
+import SkinImage from 'static/images/skin-care-img.jpg';
+
+const bodyGroup = {
+  imageSrc: BodyImage,
+  path: '/body-care',
+  groupName: 'Body Care',
+  groupDescription:
+    'Pamper your skin with our exquisite range of body care products.',
+};
+
+const hairGroup = {
+  imageSrc: HairImage,
+  path: '/hair-care',
+  groupName: 'Hair Care',
+  groupDescription:
+    'Discover the secret to luscious, healthy hair with our exceptional range of hair care products.',
+};
+
+const skinGroup = {
+  imageSrc: SkinImage,
+  path: '/skin-care-makeup',
+  groupName: 'Skin Care & Makeup',
+  groupDescription:
+    'Unleash your creativity with our exquisite makeup collection.',
+};
+
+export const groupList = [bodyGroup, hairGroup, skinGroup];
+
 export const WEBSITE_NAME = 'MARY HOANG SHOP USA';
 export const FREE_SHIPPING_MESSAGE = 'FREE SHIPPING FOR ALL STATES';
 
